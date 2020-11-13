32c7cbe2b268eb9e5756226e39ee4fbeae5247a2
Titus von der Malsburg <malsburg@posteo.de>
2020-11-13 17:38:08 +0100

    Update commentary in source code

--- txl.el
+++ txl.el
@@ -24,18 +24,18 @@
 ;; TXL provides machine translation through DeepL's REST API.
 ;; Minimally the user needs to specify a pair of languages in the
 ;; customization variable `txl-languages' and an authentication key
-;; for DeepL's REST API via `txl-deepl-api-url'.  Then the function
-;; `txl-translate-to-other-language' can be used to translate the
-;; current paragraph or the marked region to the respective other
-;; language.  (The current language is detected using
-;; `guess-language'.)  The function `txl-translate-roundtrip'
-;; translate to the other language and back, which can yield
-;; alternative, sometimes more idiomatic formulations.  Both of these
-;; functions display the translation in the minibuffer.
-;; Alternatively the functions `txl-replace-with-other-language' and
-;; `txl-replace-with-roundtrip' can be used to replace the paragraph
-;; or marked region with the translation.  Future versions may support
-;; more than two languages and other translation services.
+;; for DeepL's REST API via `txl-deepl-api-url'.
+
+;; The command `txl-translate-region-or-paragraph' translates the
+;; marked region or, if no region is active, the paragraph to the
+;; respective other language.  The current language is detected using
+;; the package guess-language.  The retrieved translation is shown in
+;; a separate buffer where it can be reviewed and edited.  The
+;; original text can be replaced with the (edited) translation via
+;; <C-c C-c>. The translation can be dismissed (without touching the
+;; original text) using <C-c C-k>. If a prefix argument is given
+;; (<C-u>), the text will be translated round-trip to the other
+;; language and back.
 
 ;;; Code:
 
